0aede48cbfc2f15e90ad00912962506ca5dc1d33
Harish A <harish18092002@gmail.com>
2023-11-06 09:58:22 +0530

    Map integration

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,18 @@
+import UIKit
+import Flutter
+import GoogleMaps  // Add this import
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+
+    // TODO: Add your Google Maps API key
+    GMSServices.provideAPIKey("AIzaSyAvKubNcSaCsSbDdl2fepQ53Q6wtuX-PSc")
+
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
